6c47af24e79f330da77641eb78a848d64be9867e
MaulanaHaqi26 <maulanahaqi3206@gmail.com>
2024-10-11 22:38:56 +0700

    update 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/views/*{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
       'xl' : '1280px',
     },
   },
