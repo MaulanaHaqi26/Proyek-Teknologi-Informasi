2072ab19a6e9627cb45e56417bd63aa1e8e1cada
MaulanaHaqi26 <maulanahaqi3206@gmail.com>
2024-10-11 13:49:46 +0700

    commit fix tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/views/*{html,js}"],
+  theme: {
+    extend: {
+      'xl' : '1280px',
+    },
+  },
+  plugins: [],
+}
